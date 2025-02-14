ee5a130ff7002b50948b3a594d1664e9846f1a18
Tishues <BradleyBounds@outlook.com>
2025-02-13 18:43:30 -0600

    H: Created unit tests for inline_markdown.py in test_inline_markdown.py

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,7 +1,7 @@
 from textnode import TextNode, TextType
 
 
-def split_node_delimiter(old_nodes, delimiter, text_type):
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:

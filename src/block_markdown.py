5d03ff1f089b92daa310efcbc593feb6445f0a53
Tishues <BradleyBounds@outlook.com>
2025-02-17 00:37:25 -0600

    N: Created block_to_block_type function

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,3 @@
-
 def markdown_to_blocks(markdown):  
     blocks = markdown.split("\n\n")
     result = []
@@ -9,3 +8,38 @@ def markdown_to_blocks(markdown):
         else:
             continue
     return result
+
+
+def block_to_block_type(markdown):
+    paragraph = "paragraph"
+    heading = "heading"
+    code = "code"
+    quote = "quote"
+    unordered_list = "unordered list"
+    ordered_list = "ordered list"
+    lines = markdown.split('\n')
+    if markdown.startswith("# "):
+        return heading
+    if markdown.startswith("## "):
+        return heading
+    if markdown.startswith("### "):
+        return heading
+    if markdown.startswith("#### "):
+        return heading
+    if markdown.startswith("##### "):
+        return heading
+    if markdown.startswith("###### "):
+        return heading
+    if markdown.startswith("```") and markdown.endswith("```"):
+        return code
+    if all([line.startswith("> ") for line in lines]):
+        return quote
+    if all([line.startswith("* ") or line.startswith("- ") for line in lines]):
+        return unordered_list
+    is_ordered_list = True
+    for i, line in enumerate(lines, 1):
+        if not line.startswith(f"{i}. "):
+            is_ordered_list = False  # Mark as not an ordered list, but keep going
+    if is_ordered_list:
+        return ordered_list
+    return paragraph
